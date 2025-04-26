8ac6d84974090c700a8f9d8d363baf1f0e1da398
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2025-04-25 20:26:24 -0400

    Use a more accurate filename

--- interpret.sh
+++ interpret.sh
@@ -2,4 +2,4 @@
 
 set -eu -o pipefail
 
-xmllint --html --xpath "concat('https://www.pghschools.org', string(//main//a/@href))" --format full.html | head -n 1 > links.html
+xmllint --html --xpath "concat('https://www.pghschools.org', string(//main//a/@href))" --format full.html | head -n 1 > url.txt
